1e2e2f15f20e6142a172a16e96d41eab454940ce
jamesw789 <jamesw789@gmail.com>
2017-11-13 09:59:51 -0500

    Add Study related pages

--- NeoWeb/Home/Controller/IndexController.class.php
+++ NeoWeb/Home/Controller/IndexController.class.php
@@ -8,63 +8,97 @@ use Home\Service\NeoService;
 use NeoWeb\Common\Common\CommonDefinition;
 use NeoWeb\Common\Common\NeoDefinition;
 
-class IndexController extends Controller {
-	
-	/*
-	 * Note: this is used old reward website
-	 * public function index()
-	 * {
-	 * session_start();
-	 * if(isset($_SESSION['uid']))
-	 * {
-	 * // header("Location: ".U('Home/User/Index'));
-	 * $this->redirect('User/index');
-	 * }
-	 * else
-	 * {
-	 * $this->show( );
-	 * }
-	 * exit();
-	 * }
-	 */
-	public function index() {
-		$this->show ();
-		exit ();
-	}
-	
-	/**
-	 * Name : getMoreServiceInfoProc
-	 * Input : N/A
-	 * Output: N/A
-	 * Description: business get more service infomation process
-	 */
-	public function getMoreServiceInfoProc() {
-		header ( "Content-type: application/json; charset = utf-8" );
-		$jsonData = json_decode ( file_get_contents ( 'php://input' ) );
-		
-		$m_neo = new NeoService ();
-		$result = $m_neo->moreServiceInfoProcess ( $jsonData );
-		
-		$this->ajaxReturn ( $result );
-		
-		exit ();
-	}
-	
-	/**
-	 * Name : getInTouchProc
-	 * Input : N/A
-	 * Output: N/A
-	 * Description: get in touch form process
-	 */
-	public function getInTouchProc() {
-		header ( "Content-type: application/json; charset = utf-8" );
-		$jsonData = json_decode ( file_get_contents ( 'php://input' ) );
-		
-		$m_neo = new NeoService ();
-		$result = $m_neo->getInTouchMsgProcess ( $jsonData );
-		
-		$this->ajaxReturn ( $result );
-		
-		exit ();
-	}
+class IndexController extends Controller
+{
+
+  /*
+   * Note: this is used old reward website
+   * public function index()
+   * {
+   * session_start();
+   * if(isset($_SESSION['uid']))
+   * {
+   * // header("Location: ".U('Home/User/Index'));
+   * $this->redirect('User/index');
+   * }
+   * else
+   * {
+   * $this->show( );
+   * }
+   * exit();
+   * }
+   */
+  public function index( )
+  {
+    $this->show();
+    exit();
+  }
+  public function about_us( )
+  {
+    $this->show();
+    exit();
+  }
+  public function contact_us( )
+  {
+    $this->show();
+    exit();
+  }
+  public function college( )
+  {
+    $this->show();
+    exit();
+  }
+  public function high_school( )
+  {
+    $this->show();
+    exit();
+  }
+  public function university( )
+  {
+    $this->show();
+    exit();
+  }
+  public function post_graduate( )
+  {
+    $this->show();
+    exit();
+  }
+
+  /**
+   * Name : getMoreServiceInfoProc
+   * Input : N/A
+   * Output: N/A
+   * Description: business get more service infomation process
+   */
+  public function getMoreServiceInfoProc( )
+  {
+    header("Content-type: application/json; charset = utf-8");
+    $jsonData = json_decode(file_get_contents('php://input'));
+
+    $m_neo = new NeoService();
+    $result = $m_neo->moreServiceInfoProcess($jsonData);
+
+    $this->ajaxReturn($result);
+
+    exit();
+  }
+
+  /**
+   * Name : getInTouchProc
+   * Input : N/A
+   * Output: N/A
+   * Description: get in touch form process
+   */
+  public function getInTouchProc( )
+  {
+    header("Content-type: application/json; charset = utf-8");
+    $jsonData = json_decode(file_get_contents('php://input'));
+
+    $m_neo = new NeoService();
+    $result = $m_neo->getInTouchMsgProcess($jsonData);
+
+    $this->ajaxReturn($result);
+
+    exit();
+  }
 }
